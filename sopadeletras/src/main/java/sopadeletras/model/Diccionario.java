b9fc71b68a0dcb9b56b7b1379676b8fd142568ac
Marcos López Collar <jlopezcoll@uoc.edu>
2021-03-19 23:49:18 +0100

    Modelo

---
+++ sopadeletras/src/main/java/sopadeletras/model/Diccionario.java
@@ -0,0 +1,34 @@
+package sopadeletras.model;
+
+public class Diccionario {
+	
+	//propiedades
+	int idPalabra;
+	String palabra;
+	
+
+	public Diccionario() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getIdPalabra() {
+		return idPalabra;
+	}
+
+
+	public void setIdPalabra(int idPalabra) {
+		this.idPalabra = idPalabra;
+	}
+
+
+	public String getPalabra() {
+		return palabra;
+	}
+
+
+	public void setPalabra(String palabra) {
+		this.palabra = palabra;
+	}
+
+}
---
+++ sopadeletras/src/main/java/sopadeletras/model/Sopa.java
@@ -0,0 +1,45 @@
+package sopadeletras.model;
+
+public class Sopa {
+	
+	//propiedades
+	int idSopa;
+	int idJugador;
+	int puntos;
+	
+
+	public Sopa() {
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public int getIdSopa() {
+		return idSopa;
+	}
+
+
+	public void setIdSopa(int idSopa) {
+		this.idSopa = idSopa;
+	}
+
+
+	public int getIdJugador() {
+		return idJugador;
+	}
+
+
+	public void setIdJugador(int idJugador) {
+		this.idJugador = idJugador;
+	}
+
+
+	public int getPuntos() {
+		return puntos;
+	}
+
+
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+
+}
