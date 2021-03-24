80b721fdb26edbd0d743a0441cb29a72ab8f7753
Marcos López Collar <jlopezcoll@uoc.edu>
2021-03-24 03:04:22 +0100

    DAO

--- sopadeletras/src/main/java/com/sopadeletras/dao/DiccionarioDAO.java
+++ sopadeletras/src/main/java/com/sopadeletras/dao/DiccionarioDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.sopadeletras.model.Diccionario;
@@ -14,7 +15,26 @@ public class DiccionarioDAO implements GenericDAO<Diccionario, Integer> {
 
 	@Override
 	public void insertar(Diccionario t) throws SQLException {
-
+		Connection conn = null;
+		PreparedStatement stat = null;
+		try {
+			conn = MySqlDB.conectar();
+			String query = "INSERT INTO diccionario (palabra) VALUES (?)";
+			stat = conn.prepareStatement(query);
+			stat.setString(1, t.getPalabra());
+			stat.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (stat != null) {
+				try {
+					stat.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}				
+			}
+			MySqlDB.desconetar();
+		}
 	}
 
 	@Override
@@ -25,46 +45,125 @@ public class DiccionarioDAO implements GenericDAO<Diccionario, Integer> {
 
 	@Override
 	public void eliminar(Diccionario t) throws SQLException {
-		// TODO Auto-generated method stub
-
+		Connection conn = null;
+		PreparedStatement stat = null;
+		try {
+			conn = MySqlDB.conectar();
+			String query = "DELETE FROM diccionario WHERE palabra = ?";
+			stat = conn.prepareStatement(query);
+			stat.setString(1, t.getPalabra());
+			stat.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (stat != null) {
+				try {
+					stat.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}				
+			}
+			MySqlDB.desconetar();
+		}
 	}
 
 	@Override
 	public List<Diccionario> obtenerTodos() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Connection conn = null;
+		PreparedStatement stat = null;
+		ResultSet rs = null;		
+		Diccionario palabra = null;
+		List<Diccionario> palabras = new ArrayList<>();
+		try {
+			conn = MySqlDB.conectar();
+			String query = "SELECT * FROM diccionario";
+			stat = conn.prepareStatement(query);
+			rs = stat.executeQuery();
+			while (rs.next()) {
+				palabra = new Diccionario(rs.getString(2));
+				palabras.add(palabra);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}				
+			}
+			if (stat != null) {
+				try {
+					stat.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}				
+			}
+			MySqlDB.desconetar();
+		}
+		return palabras;	
 	}
 
 	@Override
 	public Diccionario obtener(Integer id) throws SQLException {
 		Connection conn = null;
+		PreparedStatement stat = null;
+		ResultSet rs = null;		
 		Diccionario palabra = null;
 		try {
 			conn = MySqlDB.conectar();
 			String query = "SELECT * FROM diccionario WHERE idPalabra = ?";
-			PreparedStatement st = conn.prepareStatement(query);
-			st.setInt(1, id);
-			ResultSet rs = st.executeQuery();
+			stat = conn.prepareStatement(query);
+			stat.setInt(1, id);
+			rs = stat.executeQuery();
 			if (rs.next()) {
 				palabra = new Diccionario(rs.getString(2));
 			}
-			rs.close();
-			st.close();
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}				
+			}
+			if (stat != null) {
+				try {
+					stat.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}				
+			}
 			MySqlDB.desconetar();
 		}
-
 		return palabra;
 	}
 
+	//Pruebas de funcionamiento
 	public static void main(String[] args) throws SQLException {
 		DiccionarioDAO prueba = new DiccionarioDAO();
 		System.out.println(prueba.obtener(2).getPalabra());
 		System.out.println(prueba.obtener(5).getPalabra());
-		System.out.println(prueba.obtener(1).getPalabra());
-
+		System.out.println(prueba.obtener(3).getPalabra());
+		System.out.println("--------------------------------");
+		List<Diccionario> listPalabras = prueba.obtenerTodos();
+		for (Diccionario diccionario : listPalabras) {
+			System.out.println(diccionario.getPalabra());
+		}
+		System.out.println("--------- Insertamos una palabra -----------------------");
+		Diccionario nuevaPalabra = new Diccionario("motor");
+		prueba.insertar(nuevaPalabra);
+		//prueba.eliminar(nuevaPalabra);
+		System.out.println("--------- Listado con palabra insertada -----------------------");
+		List<Diccionario> listPalabras2 = prueba.obtenerTodos();
+		for (Diccionario diccionario : listPalabras2) {
+			System.out.println(diccionario.getPalabra());
+		}
+		
 	}
 
 }
--- sopadeletras/src/main/java/com/sopadeletras/dao/SopaDAO.java
+++ sopadeletras/src/main/java/com/sopadeletras/dao/SopaDAO.java
@@ -1,11 +1,102 @@
 package com.sopadeletras.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
 import com.sopadeletras.model.Sopa;
 
-public interface SopaDAO extends  GenericDAO<Sopa, Integer>{
+public class SopaDAO implements GenericDAO<Sopa, Integer>{
+	
+	private MySqlDB MySqlDB = new MySqlDB();
+
+	@Override
+	public void insertar(Sopa t) throws SQLException {
+		Connection conn = null;
+		PreparedStatement stat = null;
+		ResultSet rs = null;
+		try {
+			conn = MySqlDB.conectar();
+			String query = "INSERT INTO sopa (idJugador, puntos) VALUES (?,?)";
+			stat = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			stat.setInt(1, t.getIdJugador());
+			stat.setInt(2, t.getPuntos());			
+			stat.executeUpdate();
+			//Capturamos el ID de la sopa insertada
+			rs = stat.getGeneratedKeys();
+			if (rs.next()) {
+				t.setIdSopa(rs.getInt(1));
+			}
+			stat.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			MySqlDB.desconetar();
+		}
+	}
+
+	@Override
+	public void modificar(Sopa t) throws SQLException {
+		Connection conn = null;
+		PreparedStatement stat = null;
+		try {
+			conn = MySqlDB.conectar();
+			String query = "UPDATE sopa SET puntos = ? WHERE idSopa = ?";
+			stat = conn.prepareStatement(query);
+			stat.setInt(1, t.getPuntos());
+			stat.setInt(2, t.getIdSopa());
+			stat.executeUpdate();
+			stat.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			MySqlDB.desconetar();
+		}
+	}
+
+	@Override
+	public void eliminar(Sopa t) throws SQLException {
+		Connection conn = null;
+		PreparedStatement stat = null;
+		try {
+			conn = MySqlDB.conectar();
+			String query = "DELETE FROM sopa WHERE idSopa = ?";
+			stat = conn.prepareStatement(query);
+			stat.setInt(1, t.getIdSopa());		
+			stat.executeUpdate();
+			stat.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			MySqlDB.desconetar();
+		}
+	}
+	@Override
+	public List<Sopa> obtenerTodos() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Sopa obtener(Integer id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
-	
+	public static void main(String[] args) throws SQLException {
+		SopaDAO prueba = new SopaDAO();
+		Sopa nuevaSopa = new Sopa(10, 11000);
+		prueba.insertar(nuevaSopa);
+		System.out.println(nuevaSopa.getIdSopa());
+		nuevaSopa.setPuntos(0);
+		prueba.modificar(nuevaSopa);
+		//prueba
+		
+	}
 	
 	
 
