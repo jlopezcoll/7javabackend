7f949a4922f606d9a67421677b5141a454e0a115
Marcos López Collar <jlopezcoll@uoc.edu>
2021-04-26 13:42:00 +0200

    delete

--- sopadeletras/src/main/java/com/sopadeletras/model/Diccionario.java
+++ sopadeletras/src/main/java/com/sopadeletras/model/Diccionario.java
@@ -17,6 +17,7 @@ public class Diccionario implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
 	//propiedades
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
