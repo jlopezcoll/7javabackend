b83abc1cbdfab1c494e85f6750481a004c24504e
Marcos López Collar <jlopezcoll@uoc.edu>
2021-04-26 13:41:31 +0200

    Hibernate ORM

--- sopadeletras/src/main/java/com/sopadeletras/dao/SopaDAO.java
+++ sopadeletras/src/main/java/com/sopadeletras/dao/SopaDAO.java
@@ -1,122 +1,150 @@
 package com.sopadeletras.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.sql.Statement;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+//import com.sopadeletras.model.Diccionario;
 import com.sopadeletras.model.Sopa;
 
 public class SopaDAO implements GenericDAO<Sopa, Integer>{
 	
-	private MySqlDB MySqlDB = new MySqlDB();
+	//private MySqlDB MySqlDB = new MySqlDB();
+	private EntityManager manager;
+	
+	public SopaDAO() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sopadeletras");
+		manager = emf.createEntityManager();
+		}
 
 	@Override
 	public void insertar(Sopa t){
-		Connection conn = null;
-		PreparedStatement stat = null;
-		ResultSet rs = null;
-		try {
-			conn = MySqlDB.conectar();
-			String query = "INSERT INTO sopa (idJugador, puntos) VALUES (?,?)";
-			stat = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-			stat.setString(1, t.getIdJugador());
-			stat.setInt(2, t.getPuntos());			
-			stat.executeUpdate();
-			//Capturamos el ID de la sopa insertada
-			rs = stat.getGeneratedKeys();
-			if (rs.next()) {
-				t.setIdSopa(rs.getInt(1));
-			}
-			stat.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			MySqlDB.desconetar();
-		}
+		manager.getTransaction().begin();
+		manager.persist(t);
+		manager.getTransaction().commit();
+
 	}
+//	public void insertar(Sopa t){
+//		Connection conn = null;
+//		PreparedStatement stat = null;
+//		ResultSet rs = null;
+//		try {
+//			conn = MySqlDB.conectar();
+//			String query = "INSERT INTO sopa (idJugador, puntos) VALUES (?,?)";
+//			stat = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+//			stat.setString(1, t.getIdJugador());
+//			stat.setInt(2, t.getPuntos());			
+//			stat.executeUpdate();
+//			//Capturamos el ID de la sopa insertada
+//			rs = stat.getGeneratedKeys();
+//			if (rs.next()) {
+//				t.setIdSopa(rs.getInt(1));
+//			}
+//			stat.close();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			MySqlDB.desconetar();
+//		}
+//	}
 
 	@Override
 	public void modificar(Sopa t){
-		Connection conn = null;
-		PreparedStatement stat = null;
-		try {
-			conn = MySqlDB.conectar();
-			String query = "UPDATE sopa SET puntos = ? WHERE idSopa = ?";
-			stat = conn.prepareStatement(query);
-			stat.setInt(1, t.getPuntos());
-			stat.setInt(2, t.getIdSopa());
-			stat.executeUpdate();
-			stat.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			MySqlDB.desconetar();
-		}
 	}
+//	public void modificar(Sopa t){
+//		Connection conn = null;
+//		PreparedStatement stat = null;
+//		try {
+//			conn = MySqlDB.conectar();
+//			String query = "UPDATE sopa SET puntos = ? WHERE idSopa = ?";
+//			stat = conn.prepareStatement(query);
+//			stat.setInt(1, t.getPuntos());
+//			stat.setInt(2, t.getIdSopa());
+//			stat.executeUpdate();
+//			stat.close();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			MySqlDB.desconetar();
+//		}
+//	}
 
 	@Override
 	public void eliminar(Sopa t){
-		Connection conn = null;
-		PreparedStatement stat = null;
-		try {
-			conn = MySqlDB.conectar();
-			String query = "DELETE FROM sopa WHERE idSopa = ?";
-			stat = conn.prepareStatement(query);
-			stat.setInt(1, t.getIdSopa());		
-			stat.executeUpdate();
-			stat.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			MySqlDB.desconetar();
-		}
+		manager.getTransaction().begin();
+		manager.remove(t);
+		manager.getTransaction().commit();
 	}
+
+//	public void eliminar(Sopa t){
+//		Connection conn = null;
+//		PreparedStatement stat = null;
+//		try {
+//			conn = MySqlDB.conectar();
+//			String query = "DELETE FROM sopa WHERE idSopa = ?";
+//			stat = conn.prepareStatement(query);
+//			stat.setInt(1, t.getIdSopa());		
+//			stat.executeUpdate();
+//			stat.close();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			MySqlDB.desconetar();
+//		}
+//	}
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Sopa> obtenerTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		return (List<Sopa>) manager.createQuery("FROM Sopa").getResultList();
 	}
 
 	@Override
 	public Sopa obtener(Integer id){
-		Connection conn = null;
-		PreparedStatement stat = null;
-		ResultSet rs = null;		
-		Sopa sopa= null;
-		try {
-			conn = MySqlDB.conectar();
-			String query = "SELECT * FROM sopa WHERE idSopa = ?";
-			stat = conn.prepareStatement(query);
-			stat.setInt(1, id);
-			rs = stat.executeQuery();
-			if (rs.next()) {
-				sopa = new Sopa(rs.getInt(1), rs.getString(2), rs.getInt(3));
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}				
-			}
-			if (stat != null) {
-				try {
-					stat.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}				
-			}
-			MySqlDB.desconetar();
-		}
-		return sopa;
+		return manager.find(Sopa.class, id);
 	}
+//	public Sopa obtener(Integer id){
+//		Connection conn = null;
+//		PreparedStatement stat = null;
+//		ResultSet rs = null;		
+//		Sopa sopa= null;
+//		try {
+//			conn = MySqlDB.conectar();
+//			String query = "SELECT * FROM sopa WHERE idSopa = ?";
+//			stat = conn.prepareStatement(query);
+//			stat.setInt(1, id);
+//			rs = stat.executeQuery();
+//			if (rs.next()) {
+//				sopa = new Sopa(rs.getInt(1), rs.getString(2), rs.getInt(3));
+//			}
+//
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		} finally {
+//			if (rs != null) {
+//				try {
+//					rs.close();
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}				
+//			}
+//			if (stat != null) {
+//				try {
+//					stat.close();
+//				} catch (SQLException e) {
+//					e.printStackTrace();
+//				}				
+//			}
+//			MySqlDB.desconetar();
+//		}
+//		return sopa;
+//	}
 	
 	
 	/*public static void main(String[] args) throws SQLException {
--- sopadeletras/src/main/java/com/sopadeletras/model/Sopa.java
+++ sopadeletras/src/main/java/com/sopadeletras/model/Sopa.java
@@ -1,29 +1,41 @@
 package com.sopadeletras.model;
 
-public class Sopa {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="Sopa")
+public class Sopa implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	
 	//propiedades
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idSopa")
 	int idSopa;
+	@Column(name = "idJugador")
 	String idJugador;
+	@Column(name = "puntos")
 	int puntos;
 	
-
+	//Contructor vacio
 	public Sopa() {
 		
 	}	
-
+	
+	//Contructor con 2 atributos
 	public Sopa(String idJugador, int puntos) {
 		//super();
 		this.idJugador = idJugador;
 		this.puntos = puntos;
 	}
-	
-	public Sopa(int idSopa, String idJugador, int puntos) {
-		this.idSopa = idSopa;
-		this.idJugador = idJugador;
-		this.puntos = puntos;
-	}
-	
 	
 
 	public int getIdSopa() {
